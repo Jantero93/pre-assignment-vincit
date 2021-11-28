62c4b75718f0ce2864fa27db59e4a9bfbf5efd3e
Jantero93 <janzku1993@hotmail.com>
2021-11-28 08:47:22 +0200

    init navbar with css, html, selected tab functionality

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,29 +1,42 @@
 <template>
-  <div>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div id="app">
+    <Navbar
+      :selectedTab="this.$data.selectedTab"
+      @clickedTab="handleClickedTab"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+
+import Navbar from './components/Navbar.vue';
+
+interface ComponentState {
+  selectedTab: string;
+}
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Navbar
+  },
+  data() {
+    return {
+      selectedTab: 'item1'
+    } as ComponentState;
+  },
+  methods: {
+    handleClickedTab(clickedTab: string): void {
+      this.$data.selectedTab = clickedTab;
+    }
   }
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  font-family: 'Courier New', Courier, monospace;
 }
 </style>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,70 @@
+<template>
+  <nav class="navbar">
+    <a
+      v-bind:class="{ active: selectedTab === 'item1' }"
+      @click="tabClicked('item1')"
+      >item1</a
+    >
+    <a
+      v-bind:class="{ active: selectedTab === 'item2' }"
+      @click="tabClicked('item2')"
+      >item2</a
+    >
+    <a
+      v-bind:class="{ active: selectedTab === 'item3' }"
+      @click="tabClicked('item3')"
+      >item3</a
+    >
+    <a
+      v-bind:class="{ active: selectedTab === 'item4' }"
+      @click="tabClicked('item4')"
+      >item4</a
+    >
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Navbar',
+  props: {
+    selectedTab: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    tabClicked(tabName: string): void {
+      this.$emit('clickedTab', tabName);
+    }
+  }
+});
+</script>
+
+<style>
+.navbar {
+  background-color: rgb(53, 53, 53);
+  overflow: hidden;
+  position: fixed;
+  top: 0px;
+  width: 100%;
+}
+
+.navbar a {
+  float: left;
+  display: block;
+  color: #14c28e;
+  text-align: center;
+  padding: 1em 1em;
+  font-size: 18px;
+}
+
+.navbar a:hover {
+  color: white;
+}
+
+.navbar a.active {
+  background-color: black;
+}
+</style>
