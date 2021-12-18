06b715e4000056b1e023a8effbc6963cee6f2b4d
Jantero93 <janzku1993@hotmail.com>
2021-12-18 20:28:00 +0200

    new e2e tests client

---
+++ client/tests/e2e/specs/mainpage.ts
@@ -0,0 +1,34 @@
+describe('Basic functionality', () => {
+  it('Click pages', () => {
+    cy.visit('/');
+    cy.contains('Submit');
+
+    cy.contains('Description').click();
+    cy.contains('description').click();
+
+    cy.contains('About').click();
+    cy.contains('about');
+  });
+
+  it('Show alert if end date is before start date', () => {
+    cy.visit('/');
+    cy.contains('Main Content');
+
+    cy.get('#start-date').click().type('2020-09-09');
+    cy.get('#end-date').click().type('2020-01-01');
+
+    cy.on('window:alert', (message) => {
+      expect(message).toBe('Invalid input');
+    });
+  });
+
+  it('Click tabs', () => {
+    cy.visit('/');
+
+    cy.contains('Highest Trading Volume').click();
+    cy.contains('Submit');
+
+    cy.contains('Time Machine').click();
+    cy.contains('placeholder');
+  });
+});
